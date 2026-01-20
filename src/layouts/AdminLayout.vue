af670d3f762e7d4f65afff7a1a434e65425c5691
AlexSabashnikov <Sabashnikov@yandex.ru>
2026-01-20 14:46:45 +0300

    Vue.js Help Desk frontend

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,37 @@
+<!-- 
+    Макет панели администратора
+    Боковое меню с полным доступом ко всем разделам
+    Шапка с навигационной цепочкой и профилем
+-->
+
+<!--<template>
+    <div class="admin-layout">
+    <!-- Боковое меню 
+    <SidebarMenu />
+
+    <!-- Основная область 
+    <div class="main-content">
+        <!-- Шапка 
+        <Header />
+
+        <!-- Хлебные крошки 
+        <Breadcrumbs />
+
+        <!-- Контент страницы 
+        <div class="content-wrapper">
+        <slot></slot>
+        <!-- Сюда вставляется AllTickets.vue 
+        </div>
+    </div>
+
+    <!-- Всплывающие уведомления 
+    <Notification />
+    </div>
+</template>
+
+<script setup>
+import SidebarMenu from '@/components/common/AppLayout/SidebarMenu.vue'
+import Header from '@/components/common/AppLayout/Header.vue'
+import Breadcrumbs from '@/components/common/AppLayout/BreadCrumbs.vue'
+import Notification from '@/components/common/Notification.vue'
+</script>-->
